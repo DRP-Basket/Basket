b0852b4fab561b2b41936aa4ebcf8c296c5230ed
Brandon <szh19@ic.ac.uk>
2021-06-15 12:06:44 +0800

    Changed charity drawer to use pushReplacement, added donor page to charity drawer

--- frontend/lib/views/charity/charity_drawer.dart
+++ frontend/lib/views/charity/charity_drawer.dart
@@ -1,31 +1,38 @@
+import 'package:drp_basket_app/views/charity/charity_donor.dart';
 import 'package:drp_basket_app/views/charity/contact_list_page.dart';
 import 'package:flutter/material.dart';
 
 import 'charity_donation_page.dart';
 
 class CharityDrawer extends StatelessWidget {
-  const CharityDrawer({ Key? key }) : super(key: key);
+  const CharityDrawer({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Drawer(
       child: ListView(
-        padding: EdgeInsets.zero, 
+        padding: EdgeInsets.zero,
         children: [
           DrawerHeader(
             child: Text("Charity"),
           ),
-          // ListTile(
-          //   title: Text('Home'),
-          //   onTap: () => {Navigator.popAndPushNamed(context, CharityHomePage.id)},
-          // ),
           ListTile(
-            title: Text('Donation'),
-            onTap: () => {Navigator.popAndPushNamed(context, CharityDonationPage.id)}, 
+            title: Text('Events'),
+            onTap: () => {
+              Navigator.pushReplacementNamed(context, CharityDonationPage.id)
+            },
+          ),
+          ListTile(
+            title: Text('Donors'),
+            onTap: () => {
+              Navigator.pushReplacement(context,
+                  MaterialPageRoute(builder: (context) => CharityDonor()))
+            },
           ),
           ListTile(
             title: Text('Receivers'),
-            onTap: () => Navigator.popAndPushNamed(context, ContactListPage.id),
+            onTap: () =>
+                Navigator.pushReplacementNamed(context, ContactListPage.id),
           ),
         ],
       ),
